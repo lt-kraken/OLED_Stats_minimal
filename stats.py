ef86e89cd34c89e62f8bb28d13dc7e2c435f45c5
Lt.Kraken <larsnijveld@hotmail.com>
2023-08-02 18:17:35 +0200

    Update stats.py

--- stats.py
+++ stats.py
@@ -17,7 +17,7 @@ oled_reset = digitalio.DigitalInOut(board.D4)
 
 # Display Parameters
 WIDTH = 128
-HEIGHT = 64
+HEIGHT = 32
 BORDER = 5
 
 # Display Refresh
@@ -49,24 +49,17 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
+    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk>    cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.0f%%\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %.0f%%\", $3*100/$2 }'"
     MemUsage = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
-    cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
-    Temp = subprocess.check_output(cmd, shell = True )
 
     # Pi Stats Display
     draw.text((0, 0), "IP: " + str(IP,'utf-8'), font=font, fill=255)
-    draw.text((0, 16), str(CPU,'utf-8') + "LA", font=font, fill=255)
-    draw.text((80, 16), str(Temp,'utf-8') , font=font, fill=255)
-    draw.text((0, 32), str(MemUsage,'utf-8'), font=font, fill=255)
-    draw.text((0, 48), str(Disk,'utf-8'), font=font, fill=255)
+    draw.text((0, 16), str(CPU,'utf-8'), font=font, fill=255)
+    draw.text((60, 16), str(MemUsage,'utf-8'), font=font, fill=255)
         
     # Display image
     oled.image(image)
